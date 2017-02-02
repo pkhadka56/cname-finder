813c430d0b25c8476af49606f3be77c32252b917
prakash khadka <p.khadka56@yahoo.com>
2017-02-02 21:46:22 +0545

    Update read.py

--- read.py
+++ read.py
@@ -4,9 +4,9 @@ from dns.resolver import dns
 with open('subdomains.txt') as lines:
     for line in lines:
         line=line.rstrip('\n')
-            try:
-                answers = dns.resolver.query(line, 'CNAME')
-                for rdata in answers:
-                    print answers.qname, '-->', rdata.target
-            except Exception: 
-                pass
+        try:
+            answers = dns.resolver.query(line, 'CNAME')
+            for rdata in answers:
+                print answers.qname, '-->', rdata.target
+        except Exception: 
+            pass
